aabcd173836d7972e49908022347e92cbe91e5c6
Manuel Haas <finaleweisheit@gmail.com>
2012-09-25 09:40:48 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MatchRubrics::Application.config.secret_token = '439fc6adc084d8dca1f6ede7a1d831b5da9a1fe3115e94e01029dcae78e5de30d4a32faa392c2a3aa081691e8ab3d5ac8ab4ff30d93ffe9170215321acb7e0f3'
